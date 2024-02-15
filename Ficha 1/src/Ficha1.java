5904158f435a52de7041bb95531595b335395743
DiogoKeeper12 <diogo.coelho.silva@gmail.com>
2024-02-15 17:42:40 +0000

    ficha1 done

--- Ficha 1/src/Ficha1.java
+++ Ficha 1/src/Ficha1.java
@@ -7,6 +7,34 @@ public class Ficha1 {
 
     //Exercício 2
     public int maximoNumeros(int a, int b){
+        if(a>b){return a;}
+        else{return b;}
+    }
 
+    public int menorNumeros(int a,int b){
+        if(a<b){return a;}
+        else{
+            return b;
+        }
+    }
+    //Exercicio 3
+
+    //Exercicio 4
+    public double eurosParaLibras(double valor, double taxaConversao){
+        return valor*taxaConversao;
+    }
+
+    //Exercicio 5
+    public double media(double num1,double num2){
+        return (num1+num2)/2;
+    }
+
+    //Exercicio 6
+    public double factorial(int num){
+        int i,fact=1;
+        for(i=1;i<=num;i++){
+            fact=fact*i;
+        }
+        return fact;
     }
 }
--- Ficha 1/src/TestePrograma.java
+++ Ficha 1/src/TestePrograma.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.String;
 
 public class TestePrograma {
 
@@ -8,11 +9,57 @@ public class TestePrograma {
 
         Ficha1 f1 = new Ficha1(); //cria um objeto da classe ficha1 que tem os métodos necessários
 
-        //Exercicio 1
-        System.out.println("Introduza um o valor da temperatura em Celsius: ");
-        int grausCelsius = scanner.nextInt();
-        System.out.format("O respetivo valor em graus Farenheit é: %.2f°F\n",f1.celsiusParaFarenheit(grausCelsius));
-    }
+        System.out.println("Numero Ex: 1,2,3,4,5,6,7: \n");
+        int exercicio = scanner.nextInt();
 
-        //Exercício 2
+        switch(exercicio){
+            case 1:
+                //Exercicio 1
+                System.out.println("Introduza um o valor da temperatura em Celsius: ");
+                int grausCelsius = scanner.nextInt();
+                System.out.format("O respetivo valor em graus Farenheit é: %.2f°F\n",f1.celsiusParaFarenheit(grausCelsius));
+                break;
+            case 2:
+                //Exericicio 2
+                System.out.println("Introduza dois números: ");
+                int num1 = scanner.nextInt();
+                int num2 = scanner.nextInt();
+                System.out.format("O maior número é: %d", f1.maximoNumeros(num1,num2));
+                break;
+
+
+            case 3:
+                //Exercicio 3
+                System.out.println("Introduza o seu nome: ");
+                String nome = scanner.next();
+
+                 System.out.println("Introduza o seu saldo bancário:");
+                 double saldo = scanner.nextDouble();
+                System.out.format("Olá %s, tem %f€ na conta.",nome, saldo);
+
+
+            case 4:
+                //Exercicio 4
+                System.out.println("Introduza um valor em Euros: ");
+                int valor = scanner.nextInt();
+                System.out.println("Introduza uma taxa de conversão: ");
+                double taxaConversao = scanner.nextDouble();
+                System.out.format("O valor em libras é: %.2f£", f1.eurosParaLibras(valor,taxaConversao));
+
+            case 5:
+                //Exercicio 5
+                System.out.println("Introduza dois números: ");
+                int valor1 = scanner.nextInt();
+                int valor2 = scanner.nextInt();
+                System.out.format("%d %d, média: %.2f",f1.maximoNumeros(valor1, valor2),f1.menorNumeros(valor1,valor2),f1.media(valor1,valor2));
+
+            case 6:
+                System.out.println("Introduza um número: ");
+                int nume = scanner.nextInt();
+                System.out.format("O factorial de %d é: %f",nume,f1.factorial(nume));
+
+            case 7:
+                    //acabar em casa!!!!!
+        }
+    }
 }
